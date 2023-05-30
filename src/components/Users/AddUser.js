b0dfb70395a32e59309aa60971311a794a9b7ee0
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2023-05-30 20:03:35 +0530

    user-mgmt-ui

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,18 +4,27 @@ import Card from '../UI/Card';
 import Button from '../UI/Button';
 
 import classes from './AddUser.module.css';
-
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
     const [userName, setUserName] = useState('');
     const [userAge, setUserAge] = useState('');
+    const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
         if(userName.trim().length === 0 || userAge.trim().length === 0) {
+            setError({
+                title: 'Invalid Input',
+                message: 'Please enter a valid name and age (non-empty values).'
+            });
             return;
         }
         if(+userAge < 1) {
+            setError({
+                title: 'Invalid Age',
+                message: 'Please enter a valid age( > 0).'
+            });
             return;
         }
         props.onAddUser(userName, userAge);
@@ -33,8 +42,14 @@ const AddUser = (props) => {
         console.log(event.target.value);
     };
 
+    const errorHandler = () => {
+        setError(null);
+    };
+
     return (
+      
         <Card className = {classes.input}>
+        {error && <ErrorModal title = {error.title} message = {error.message} onConfirm = {errorHandler}/>}
         <form onSubmit = {addUserHandler}>
             <label htmlFor = "username">Username</label>
             <input id = "username" type = "text" value = {userName} onChange = {userNameChangeHandler}/>
