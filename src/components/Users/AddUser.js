aa6898061e0d658781fe636ffe430b2edf6d4d74
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2023-06-03 20:46:04 +0530

    code

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -5,6 +5,7 @@ import Button from '../UI/Button';
 
 import classes from './AddUser.module.css';
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = (props) => {
     const [userName, setUserName] = useState('');
@@ -47,7 +48,7 @@ const AddUser = (props) => {
     };
 
     return (
-      
+        <Wrapper>
         <Card className = {classes.input}>
         {error && <ErrorModal title = {error.title} message = {error.message} onConfirm = {errorHandler}/>}
         <form onSubmit = {addUserHandler}>
@@ -57,7 +58,7 @@ const AddUser = (props) => {
             <label htmlFor = "age">Age (Years)</label>
             <input id = "age" type = "number" value = {userAge} onChange = {userAgeChangeHandler}/>
             <Button type = "submit">Add User</Button>
-        </form></Card>);
+        </form></Card></Wrapper>);
 };
 
 export default AddUser;
